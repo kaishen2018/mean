aa3ccbce692a458fe3b36713abcfc37354df0640
yangguoyk2015 <ywkmc@163.com>
2016-10-18 23:04:01 +0800

    fix js lint errors.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//server.js
+// server.js
 var path = require('path');
 var express = require('express');
 var app = express();
@@ -21,16 +21,16 @@ var todoModel = mongoose.model('todo', {
   text: String
 });
 
-//configuration
+// configuration
 // app.configure(function () {
 // find static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname.join('/public')));
 
-//display a log of all requests on console.
+// display a log of all requests on console.
 // app.use(express.logger('dev'));
 app.use(morgan('dev'));
 
-//change html post method
+// change html post method
 // app.use(express.bodyParser());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
@@ -39,7 +39,7 @@ app.use(bodyParser.json());
 
 // Routes of out API  -- start ------
 app.get('/api/todos', function (req, res) {
-  setTimeout( function() {
+  setTimeout(function () {
     todoModel.find(function (error, todos) {
       if (error) {
         res.send(error);
@@ -50,8 +50,8 @@ app.get('/api/todos', function (req, res) {
 });
 
 app.post('/api/todos', function (req, res) {
-  setTimeout( function () {
-    console.log( 'req body: ',  JSON.stringify(req.body));
+  setTimeout(function () {
+    console.log('req body: ', JSON.stringify(req.body));
     todoModel.create({
       text: req.body.text,
       done: false
@@ -65,7 +65,8 @@ app.post('/api/todos', function (req, res) {
         }
         res.json(todos);
       });
-  }) }, 5000);
+    })
+  }, 5000);
 
   ;
   // });
@@ -89,10 +90,8 @@ app.post('/api/todos', function (req, res) {
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
   })
-
 });
 // Routes of out API  -- End ------
-
 
 // start listening port
 app.listen(8888, function () {
