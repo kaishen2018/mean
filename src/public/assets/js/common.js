23a0a88322b90aa0a1f1269d5febf22f43242b3d
LemonYang7777 <yoko_yang2011@hotmail.com>
2016-11-23 14:22:12 +0800

    Add User UI

---
+++ src/public/assets/js/common.js
@@ -0,0 +1,29 @@
+﻿Date.prototype.format = function (format) {
+    var args = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),  //quarter
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(format))
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var i in args) {
+        var n = args[i];
+        if (new RegExp("(" + i + ")").test(format))
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? n : ("00" + n).substr(("" + n).length));
+    }
+    return format;
+};
+
+common.getMonday = function (offset) {
+    var d = new Date(); return new Date(d - ((d.getDay() || 7) - 1 - (offset || 0) * 7) * 864E5);
+}
+Array.prototype.remove = function (val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+};
---
+++ src/public/controllers/user.js
@@ -0,0 +1,79 @@
+﻿function userController($scope, $http, $state, $stateParams) {
+    $scope.users = {};
+    $scope.user = {};
+    // when the page loads, all API calls users
+    $http.get('http://localhost:8888/api/user')
+      .success(function (data) {
+          $scope.users = data;
+          $scope.genderSelect = [{ name: "男", id: '1' },
+              { name: "女", id: '0' }]
+          $scope.user.gender = '1';
+          console.log(data);
+      })
+      .error(function (err) {
+          console.log('Error: ', err);
+      });
+
+
+    // when a new user added, send the text to API
+    $scope.addUser = function () {
+        $http.post('http://localhost:8888/api/user', $scope.user)
+          .success(function (data) {
+              $scope.user = {};
+              $scope.users = data;
+              $state.go('user');
+          })
+          .error(function (err) {
+              console.log('Error :', err);
+          });
+    };
+
+    // when  user updated, send the text to API
+    $scope.updateUser = function () {
+        $http.put('http://localhost:8888/api/user', $scope.user)
+          .success(function (data) {
+              $scope.user = {};
+              $scope.users =data;
+              $state.go('user');
+          })
+          .error(function (err) {
+              console.log('Error :', err);
+          });
+    };
+
+    // Delete a user
+    $scope.deleteUser = function (id) {
+        $http.delete('http://localhost:8888/api/user/' + id)
+          .success(function (data) {
+              $scope.user = {};
+              $scope.users = data;
+              $state.go('user');
+          })
+          .error(function (err) {
+              console.log('Error : ', err);
+          })
+    };
+
+    $scope.showMsg = function (msg) {
+        alert('msg');
+    };
+
+    $scope.gotoUserDetail = function (id) {
+        $state.go('user.detail', { userId: id });
+    }
+
+    // get detail of one user
+    $scope.getUser = function () {
+        var id = $stateParams.userId
+        $http.get('http://localhost:8888/api/user/' + id)
+          .success(function (data) {
+              $scope.user = data;
+              $scope.user.gender = JSON.stringify(data.gender);
+              $scope.user.birthday = new Date(data.birthday);
+              console.log(data)
+          })
+          .error(function (err) {
+              console.log('Error :', err);
+          });
+    };
+}
